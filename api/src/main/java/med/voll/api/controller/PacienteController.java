6d8a2a4d699e25d1c0c9a06e5f7694ec60ca38b8
WellSCosta <wellscostaofc@gmail.com>
2025-02-18 13:51:18 -0300

    Tratador de Erros

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,11 @@
 package med.voll.api.controller;
 
-import med.voll.api.paciente.DadosCadastroPaciente;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import med.voll.api.domain.paciente.DadosCadastroPaciente;
 
 @RestController
 @RequestMapping("pacientes")
